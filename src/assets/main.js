4fb68b69c1559d1adb298bd21569a9820b39decc
FabioSungurlian <31576185+FabioSungurlian@users.noreply.github.com>
2017-12-21 14:52:21 -0200

    primer intento
    primer intento de programación, posterior a batallar por conseguir hacer funcionar github.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: "https://www.codeschool.com/users/Fabio_Sungurlian.json",
+    dataType: "jsonp",
+    success(response){
+      for(let course of response.courses.completed){
+        let courseDiv = $(`<div class="course">
+          <h3>${course.title}</h3>
+          <img src="${course.badge}">
+          <a href="${course.url}" target="_blank" class="btn btn-primary"> See Course</a>
+        </div>`);
+        $("#badges").append(courseDiv);
+      }
+    }
+  });
 });
